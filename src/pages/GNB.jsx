55fc2a84a9fa1138ab40fe0f4d42d13dfb6e9994
dlwjdals <jml22zzang@naver.com>
2022-06-20 16:04:08 +0900

    Add : GNB - 구현중

---
+++ src/pages/GNB.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function GNB() {
+  return <Body></Body>;
+}
+
+const Body = styled.div`
+  /* width: 100vw; */
+  height: 10vh;
+  background-color: blue;
+`;
