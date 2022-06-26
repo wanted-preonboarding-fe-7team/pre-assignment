de9f2711741ff368bb8822242af7fec794529e0d
dlwjdals <jml22zzang@naver.com>
2022-06-26 09:52:10 +0900

    Fix : Login - Assignment3 구현 중...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,20 +3,242 @@ import styled from 'styled-components';
 import { AiFillFacebook } from 'react-icons/ai';
 import { AiFillApple } from 'react-icons/ai';
 import { FaGooglePlay } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
 
 export default function Login() {
+  const [loginOutLine, setLoginOutLine] = useState('rgba(0,0,0, 0.2)');
+  const [passwordOutLine, setPasswordOutLine] = useState('rgba(0,0,0, 0.2)');
+
+  const CheckEmail = (e) => {
+    if (e.key === 'Enter') {
+      const ID = e.target.value;
+      console.log(ID);
+      if (ValidationEmail(ID)) {
+        console.log(loginOutLine);
+        setLoginOutLine('rgba(0,0,0, 0.2)');
+        // setOpac(0);
+      } else if (!ValidationEmail(ID)) {
+        setLoginOutLine('red');
+        // setOpac(100);
+      }
+    }
+  };
+
+  const CheckPassword = (e) => {
+    if (e.key === 'Enter') {
+      const PW = e.target.value;
+      console.log(PW);
+      if (ValidationPassword(PW)) {
+        setPasswordOutLine('rgba(0,0,0, 0.2)');
+        // setOpac(0);
+      } else if (!ValidationPassword(PW)) {
+        setPasswordOutLine('red');
+        // setOpac(100);
+      }
+    }
+  };
+
+  const ValidationEmail = (e) => {
+    return /^[A-Za-z0-9.\-_]+@([A-Za-z0-9-]+\.)+[A-Za-z]{0,}$/.test(e);
+  };
+
+  const ValidationPassword = (e) => {
+    return /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{8,}/.test(e);
+  };
+
+  const Body = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: auto;
+    height: 100vh;
+    width: 100vw;
+  `;
+
+  const Bundle = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: auto;
+  `;
+
+  const LoginPage = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 50px;
+    outline: rgba(0, 0, 0, 0.2) 1px solid;
+    background-color: white;
+  `;
+
+  const Instagram = styled.div`
+    font-size: 35px;
+    font-family: 'Petit Formal Script', cursive;
+    font-weight: bold;
+    margin: 50px 0;
+  `;
+
+  const LoginInput = styled.input`
+    margin: 5px 0;
+    width: 20vw;
+    min-width: 320px;
+    outline: 1px solid ${loginOutLine};
+    background-color: rgba(218, 218, 218, 0.2);
+    padding: 10px;
+    border-radius: 5px;
+  `;
+
+  const PasswordInput = styled.input`
+    margin: 5px 0;
+    width: 20vw;
+    min-width: 320px;
+    outline: 1px solid ${passwordOutLine};
+    background-color: rgba(218, 218, 218, 0.2);
+    padding: 10px;
+    border-radius: 5px;
+  `;
+
+  const Button = styled.button`
+    cursor: pointer;
+    margin: 20px 0;
+    padding: 8px;
+    width: 20vw;
+    min-width: 320px;
+    color: white;
+    background-color: rgba(187, 224, 252, 1);
+    border-radius: 5px;
+    font-weight: bold;
+  `;
+
+  const Button2 = styled.button`
+    cursor: pointer;
+    margin: 20px 0;
+    padding: 8px;
+    width: 20vw;
+    min-width: 320px;
+    color: white;
+    background-color: #37a2f5;
+    border-radius: 5px;
+    font-weight: bold;
+  `;
+
+  const OrBar = styled.div`
+    display: flex;
+    width: 100%;
+  `;
+
+  const Hr = styled.hr`
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    width: 40%;
+  `;
+
+  const Or = styled.div`
+    color: rgba(0, 0, 0, 0.4);
+    font-weight: bold;
+  `;
+
+  const Facebook = styled.div`
+    cursor: pointer;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+    font-size: 15px;
+    font-weight: bold;
+    color: rgba(56, 81, 133, 1);
+  `;
+
+  const ForgotPassword = styled.div`
+    &:hover {
+      text-decoration: underline;
+    }
+    cursor: pointer;
+    color: rgba(56, 81, 133, 1);
+    font-size: 12px;
+    font-weight: bold;
+  `;
+
+  const SignInPage = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+    padding: 20px 0;
+    outline: 1px solid rgba(0, 0, 0, 0.2);
+    width: 100%;
+    font-size: 14px;
+  `;
+
+  const SignIn = styled.span`
+    &:hover {
+      text-decoration: underline;
+    }
+    cursor: pointer;
+    color: rgb(0, 149, 246);
+    font-weight: bold;
+  `;
+
+  const DownloadDescription = styled.div`
+    font-size: 14px;
+    font-weight: bold;
+  `;
+
+  const Downloads = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    padding: 10px;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  `;
+
+  const DownloadButton = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: center;
+    margin: 10px;
+    padding: 5px;
+    width: 180px;
+    border-radius: 8px;
+    color: white;
+    background-color: black;
+  `;
+
+  const DownloadDescription2 = styled.div`
+    margin: 0 10px;
+  `;
+
+  const DoDownload = styled.div`
+    font-size: 11px;
+  `;
+
   return (
     <Body>
       <Bundle>
         <LoginPage>
           <Instagram>Instagram</Instagram>
-          <Input
+          <LoginInput
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             autoFocus
+            onKeyDown={CheckEmail}
           />
-          <Input type="text" placeholder="비밀번호" />
-          <Button>로그인</Button>
+          <PasswordInput
+            type="text"
+            placeholder="비밀번호"
+            onKeyDown={CheckPassword}
+          />
+          {loginOutLine === 'rgba(0,0,0, 0.2)' &&
+          passwordOutLine === 'rgba(0,0,0, 0.2)' ? (
+            <Button2>로그인</Button2>
+          ) : (
+            <Button>로그인</Button>
+          )}
           <OrBar>
             <Hr />
             <Or>또는</Or>
@@ -29,7 +251,7 @@ export default function Login() {
           <ForgotPassword>비밀번호를 잊으셨나요?</ForgotPassword>
         </LoginPage>
         <SignInPage>
-          계정이 없으신가요? <SignIn>&nbsp;가입하기</SignIn>
+          계정이 없으신가요?&nbsp;<SignIn>가입하기</SignIn>
         </SignInPage>
         <DownloadDescription>앱을 다운로드하세요.</DownloadDescription>
         <Downloads>
@@ -50,141 +272,3 @@ export default function Login() {
     </Body>
   );
 }
-
-const Body = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  margin: auto;
-  height: 100vh;
-  width: 100vw;
-`;
-
-const Bundle = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  margin: auto;
-`;
-
-const LoginPage = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 50px;
-  outline: rgba(0, 0, 0, 0.2) 1px solid;
-  background-color: white;
-`;
-
-const Instagram = styled.div`
-  font-size: 35px;
-  font-family: 'Petit Formal Script', cursive;
-  font-weight: bold;
-  margin: 50px 0;
-`;
-
-const Input = styled.input`
-  margin: 5px 0;
-  width: 20vw;
-  min-width: 320px;
-  outline: 1px solid rgba(0, 0, 0, 0.2);
-  background-color: rgba(218, 218, 218, 0.2);
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  margin: 20px 0;
-  padding: 8px;
-  width: 20vw;
-  min-width: 320px;
-  color: white;
-  background-color: rgba(187, 224, 252, 1);
-  border-radius: 5px;
-  font-weight: bold;
-`;
-
-const OrBar = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-const Hr = styled.hr`
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  width: 40%;
-`;
-
-const Or = styled.div`
-  color: rgba(0, 0, 0, 0.4);
-  font-weight: bold;
-`;
-
-const Facebook = styled.div`
-  cursor: pointer;
-  margin: 20px 0;
-  display: flex;
-  align-items: center;
-  font-size: 15px;
-  font-weight: bold;
-  color: rgba(56, 81, 133, 1);
-`;
-
-const ForgotPassword = styled.div`
-  cursor: pointer;
-  color: rgba(56, 81, 133, 1);
-  font-size: 12px;
-  font-weight: bold;
-`;
-
-const SignInPage = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 20px 0;
-  padding: 20px 0;
-  outline: 1px solid rgba(0, 0, 0, 0.2);
-  width: 100%;
-  font-size: 14px;
-`;
-
-const SignIn = styled.span`
-  cursor: pointer;
-  color: rgb(0, 149, 246);
-  font-weight: bold;
-`;
-
-const DownloadDescription = styled.div`
-  font-size: 14px;
-  font-weight: bold;
-`;
-
-const Downloads = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  padding: 10px;
-`;
-
-const DownloadButton = styled.div`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: center;
-  margin: 10px;
-  padding: 5px;
-  width: 180px;
-  border-radius: 8px;
-  color: white;
-  background-color: black;
-`;
-
-const DownloadDescription2 = styled.div`
-  margin: 0 10px;
-`;
-
-const DoDownload = styled.div`
-  font-size: 11px;
-`;
