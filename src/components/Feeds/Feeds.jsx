450c6a2a85403fcda6414eb65239861257b4a3d1
BB-choi <choibobae0809@gmail.com>
2022-06-26 18:37:03 +0900

    fix: Feeds 첫 화면 오류 해결
    - loading spinner 표시 전 "게시물이 없습니다" 메시지 표시되던 오류 해결

--- src/components/Feeds/Feeds.jsx
+++ src/components/Feeds/Feeds.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import Loading from 'components/Loading';
 import getFeedData from 'service/feedApi';
-import { after } from 'utils/utils';
+import { after, isEmpty } from 'utils/utils';
 
 import Feed from './Feed';
 import FeedsWrapper from './Feeds.styled';
@@ -23,31 +23,33 @@ const Feeds = () => {
   useEffect(() => {
     fetchFeedData();
   }, []);
+
   return (
     <FeedsWrapper>
       {(feedData && (
         <>
           {!imagesOnLoad && <Loading />}
-          {feedData.feedItems.map(({ id, ...data }) => {
-            const [currentFeedLiked] = feedData.liked.filter(
-              (data) => data.id === id
-            );
-            const [currentFeedComments] = feedData.comments.filter(
-              (data) => data.id === id
-            );
+          {(!isEmpty(feedData.feedItems) &&
+            feedData.feedItems.map(({ id, ...data }) => {
+              const [currentFeedLiked] = feedData.liked.filter(
+                (data) => data.id === id
+              );
+              const [currentFeedComments] = feedData.comments.filter(
+                (data) => data.id === id
+              );
 
-            return (
-              <Feed
-                key={id}
-                data={data}
-                liked={currentFeedLiked}
-                comments={currentFeedComments}
-                onLoad={handleImagesOnLoad}
-              />
-            );
-          })}
+              return (
+                <Feed
+                  key={id}
+                  data={data}
+                  liked={currentFeedLiked}
+                  comments={currentFeedComments}
+                  onLoad={handleImagesOnLoad}
+                />
+              );
+            })) || <p>등록된 게시물이 없습니다.</p>}
         </>
-      )) || <p>등록된 게시물이 없습니다.</p>}
+      )) || <Loading />}
     </FeedsWrapper>
   );
 };
